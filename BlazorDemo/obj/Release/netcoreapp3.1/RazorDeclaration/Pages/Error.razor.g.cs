4d4cae595a3545aade42210e8515ff75fe19b854
emanuele.bambini@macoev.com <emanuele.bambini@macoev.com>
2022-04-27 00:55:46 +0200

    Update

--- BlazorDemo/obj/Release/netcoreapp3.1/RazorDeclaration/Pages/Error.razor.g.cs
+++ BlazorDemo/obj/Release/netcoreapp3.1/RazorDeclaration/Pages/Error.razor.g.cs
@@ -13,63 +13,63 @@ namespace BlazorDemo.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\BlazorDemo\_Imports.razor"
+#line 1 "C:\Users\emanu\source\Repos\EmanueleBambini\BlazorDemo\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\BlazorDemo\_Imports.razor"
+#line 2 "C:\Users\emanu\source\Repos\EmanueleBambini\BlazorDemo\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\BlazorDemo\_Imports.razor"
+#line 3 "C:\Users\emanu\source\Repos\EmanueleBambini\BlazorDemo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\BlazorDemo\_Imports.razor"
+#line 4 "C:\Users\emanu\source\Repos\EmanueleBambini\BlazorDemo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\BlazorDemo\_Imports.razor"
+#line 5 "C:\Users\emanu\source\Repos\EmanueleBambini\BlazorDemo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\BlazorDemo\_Imports.razor"
+#line 6 "C:\Users\emanu\source\Repos\EmanueleBambini\BlazorDemo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\BlazorDemo\_Imports.razor"
+#line 7 "C:\Users\emanu\source\Repos\EmanueleBambini\BlazorDemo\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\BlazorDemo\_Imports.razor"
+#line 8 "C:\Users\emanu\source\Repos\EmanueleBambini\BlazorDemo\_Imports.razor"
 using BlazorDemo;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\BlazorDemo\_Imports.razor"
+#line 9 "C:\Users\emanu\source\Repos\EmanueleBambini\BlazorDemo\_Imports.razor"
 using BlazorDemo.Shared;
 
 #line default
--- ToDoList/obj/Release/netcoreapp3.1/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ ToDoList/obj/Release/netcoreapp3.1/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,70 +13,70 @@ namespace ToDoList.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\ToDoList\_Imports.razor"
+#line 1 "C:\Users\emanu\source\Repos\EmanueleBambini\ToDoList\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\ToDoList\_Imports.razor"
+#line 2 "C:\Users\emanu\source\Repos\EmanueleBambini\ToDoList\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\ToDoList\_Imports.razor"
+#line 3 "C:\Users\emanu\source\Repos\EmanueleBambini\ToDoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\ToDoList\_Imports.razor"
+#line 4 "C:\Users\emanu\source\Repos\EmanueleBambini\ToDoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\ToDoList\_Imports.razor"
+#line 5 "C:\Users\emanu\source\Repos\EmanueleBambini\ToDoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\ToDoList\_Imports.razor"
+#line 6 "C:\Users\emanu\source\Repos\EmanueleBambini\ToDoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\ToDoList\_Imports.razor"
+#line 7 "C:\Users\emanu\source\Repos\EmanueleBambini\ToDoList\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\ToDoList\_Imports.razor"
+#line 8 "C:\Users\emanu\source\Repos\EmanueleBambini\ToDoList\_Imports.razor"
 using ToDoList;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\ToDoList\_Imports.razor"
+#line 9 "C:\Users\emanu\source\Repos\EmanueleBambini\ToDoList\_Imports.razor"
 using ToDoList.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\ToDoList\Pages\FetchData.razor"
+#line 3 "C:\Users\emanu\source\Repos\EmanueleBambini\ToDoList\Pages\FetchData.razor"
 using ToDoList.Data;
 
 #line default
@@ -91,7 +91,7 @@ using ToDoList.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 39 "C:\Users\emanu\Source\Repos\MaCoEv\emanuele.bambini-Blazor\ToDoList\Pages\FetchData.razor"
+#line 39 "C:\Users\emanu\source\Repos\EmanueleBambini\ToDoList\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
