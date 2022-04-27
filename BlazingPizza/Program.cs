7f7a495dcf165855aa48b4c5e0737de851f89f37
emabambo <80698232+emabambo@users.noreply.github.com>
2022-04-28 01:14:57 +0200

    clone git BlazingPizza con grafica

--- BlazingPizza/Program.cs
+++ BlazingPizza/Program.cs
@@ -1,27 +1,19 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using BlazingPizza.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
 
-
 app.UseStaticFiles();
-
 app.UseRouting();
 
+app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
